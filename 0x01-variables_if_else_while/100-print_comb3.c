e49fd9ddb74c628ee7a2f5156f0dfb97b70a0678
xola-mthembu <xola.mthembu@gmail.com>
2023-07-15 07:20:06 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+int tens_digit = 0;
+int ones_digit;
+while (tens_digit <= 8)
+{
+ones_digit = tens_digit + 1;
+while (ones_digit <= 9)
+{
+putchar(tens_digit + '0');
+putchar(ones_digit + '0');
+if (tens_digit != 8 || ones_digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+ones_digit++;
+}
+tens_digit++;
+}
+putchar('\n');
+return (0);
+}

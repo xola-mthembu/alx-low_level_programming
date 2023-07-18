e55bc0a293cb7e2ea07cad66f73f05b44435ea89
xola-mthembu <xola.mthembu@gmail.com>
2023-07-18 12:47:06 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_putchar('_'); /* Print '_' */
+_putchar('p'); /* Print 'p' */
+_putchar('u'); /* Print 'u' */
+_putchar('t'); /* Print 't' */
+_putchar('c'); /* Print 'c' */
+_putchar('h'); /* Print 'h' */
+_putchar('a'); /* Print 'a' */
+_putchar('r'); /* Print 'r' */
+_putchar('\n'); /* Print a newline character */
+return (0); /* Return 0 to indicate successful execution */
+}

5f9dc87b765182b59f53b4e6dae100214988827d
xola-mthembu <xola.mthembu@gmail.com>
2023-07-18 15:06:40 -0700

    Q9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,20 +8,15 @@ int row, column, product;
 for (row = 0; row <= 9; row++)
 {
 _putchar('0');
-_putchar(',');
-_putchar(' ');
 for (column = 1; column <= 9; column++)
 {
 product = row * column;
+_putchar(',');
+_putchar(' ');
 if (product <= 9)
 _putchar(' ');
 _putchar(product / 10 + '0');
 _putchar(product % 10 + '0');
-if (column != 9)
-{
-_putchar(',');
-_putchar(' ');
-}
 }
 _putchar('\n');
 }

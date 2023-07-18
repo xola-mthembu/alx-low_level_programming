88c7acb7b673c9c757a0f50b9af7a422a3302532
xola-mthembu <xola.mthembu@gmail.com>
2023-07-18 14:39:50 -0700

    Q7

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_last_digit(98);
+	print_last_digit(0);
+	print_last_digit(-1024);
+	_putchar('\n');
+	return (0);
+}

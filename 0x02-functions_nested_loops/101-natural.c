df977edcd23da99c5855c1aaab1b35effc3a9aae
xola-mthembu <xola.mthembu@gmail.com>
2023-07-18 16:16:48 -0700

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int i, sum; /* Declare variables for iteration and sum */
 sum = 0; /* Initialize sum to 0 */
 /* Iterate over numbers below 1024 */
-for (i = 0; i < 1024; i++) {
-if (i % 3 == 0 || i % 5 == 0) {
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
 sum += i; /* Add the number to the sum if it is a multiple of 3 or 5 */
 }
 }
 printf("%d\n", sum); /* Print the sum */
-return 0;
+return (0);
 }
-

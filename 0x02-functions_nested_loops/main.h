02b5123d3274a6db3015610f79a7a66d5c234899
xola-mthembu <xola.mthembu@gmail.com>
2023-07-18 13:07:50 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,8 @@
 #define MAIN_H
 int _putchar(char c); /* Function prototype for _putchar */
 #endif /* MAIN_H */
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c); /* Function prototype for _putchar */
+void print_alphabet(void); /* Function prototype for print_alphabet */
+#endif /* MAIN_H */
---
+++ README.md
@@ -0,0 +1,1 @@
+C - Let's goooo!

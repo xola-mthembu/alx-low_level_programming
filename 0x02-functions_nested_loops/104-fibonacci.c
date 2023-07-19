9013d3ca28f64ff8b05b4480080799cfbeda72bd
xola-mthembu <xola.mthembu@gmail.com>
2023-07-18 17:01:12 -0700

    Q16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,14 +6,17 @@
  */
 int main(void)
 {
-int i, n1 = 1, n2 = 2, next;
-printf("1, 2, ");
+int i;
+unsigned long int fib1, fib2, fib3;
+fib1 = 1; /* First Fibonacci number */
+fib2 = 2; /* Second Fibonacci number */
+printf("%lu, %lu", fib1, fib2); /* Print the first two Fibonacci numbers */
 for (i = 3; i <= 98; i++)
 {
-next = n1 + n2;
-printf("%d, ", next);
-n1 = n2;
-n2 = next;
+fib3 = fib1 + fib2; /* Calculate the next Fibonacci number */
+printf(", %lu", fib3); /* Print the Fibonacci number */
+fib1 = fib2; /* Update the previous Fibonacci number */
+fib2 = fib3; /* Update the current Fibonacci number */
 }
 printf("\n");
 return (0);

0339fa07d18338a044ec013ed2c76e2d70315657
xola-mthembu <xola.mthembu@gmail.com>
2023-07-19 12:01:46 -0700

    Q0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - tests function that prints if integer is positive, negative, or zero
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0; /* Change this line to test different values */
+positive_or_negative(i);
+return (0);
+}

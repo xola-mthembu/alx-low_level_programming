6e5eedc0bfeb9858fa177413e9e0db0111e57f31
xola-mthembu <xola.mthembu@gmail.com>
2023-07-19 13:37:37 -0700

    Q3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,9 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
-if (month > 2)
+if (month > 2 && day >= 60)
 {
 day++;
 }
@@ -29,4 +29,6 @@ else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
+}
+}
 }

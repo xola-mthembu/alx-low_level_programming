5eccd95a3cc7b63a40ba139368b0112f612a7be7
xola-mthembu <xola.mthembu@gmail.com>
2023-07-18 17:08:37 -0700

    Q16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,17 +6,33 @@
  */
 int main(void)
 {
-int i;
-unsigned long int fib1, fib2, fib3;
-fib1 = 1; /* First Fibonacci number */
-fib2 = 2; /* Second Fibonacci number */
-printf("%lu, %lu", fib1, fib2); /* Print the first two Fibonacci numbers */
-for (i = 3; i <= 98; i++)
+unsigned long int i;
+unsigned long int bef = 1;
+unsigned long int aft = 2;
+unsigned long int l = 1000000000;
+unsigned long int bef1;
+unsigned long int bef2;
+unsigned long int aft1;
+unsigned long int aft2;
+printf("%lu", bef);
+for (i = 1; i < 91; i++)
 {
-fib3 = fib1 + fib2; /* Calculate the next Fibonacci number */
-printf(", %lu", fib3); /* Print the Fibonacci number */
-fib1 = fib2; /* Update the previous Fibonacci number */
-fib2 = fib3; /* Update the current Fibonacci number */
+printf(", %lu", aft);
+aft += bef;
+bef = aft - bef;
+}
+bef1 = bef / l;
+bef2 = bef % l;
+aft1 = aft / l;
+aft2 = aft % l;
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", aft1 + aft2 / l);
+printf(", %lu", aft2 % l);
+aft1 = aft1 + bef1;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
 }
 printf("\n");
 return (0);

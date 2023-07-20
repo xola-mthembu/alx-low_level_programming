f49f77cb1e1545a3d14f3bd597fca0054595bdd2
xola-mthembu <xola.mthembu@gmail.com>
2023-07-20 11:57:57 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*fizz_buzz - Prints numbers from 1 to 100, replacing multiples of 3 with Fizz,
+*multiples of 5 with Buzz, and both 3 and 5 with FizzBuzz.
+*/
+void fizz_buzz(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+}

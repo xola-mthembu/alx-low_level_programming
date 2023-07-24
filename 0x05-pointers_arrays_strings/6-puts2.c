81d900619a746260a9164f96bb1d917a4860d7b8
xola-mthembu <xola.mthembu@gmail.com>
2023-07-24 14:28:02 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - Prints every other character of a string.
+ * @str: Pointer to the string to be printed.
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+int index = 0;
+while (str[index] != '\0')
+{
+_putchar(str[index]);
+index += 2; /* Move to the next character (skip one character) */
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ int _strlen(char *s);
 int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
 
 #endif /* MAIN_H */

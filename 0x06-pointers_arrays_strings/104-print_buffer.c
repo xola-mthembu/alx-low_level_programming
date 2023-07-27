e97025ea9a3e7400a9800fc501ba81e21f7fe2ec
xola-mthembu <xola.mthembu@gmail.com>
2023-07-27 10:28:10 -0700

    ./104-buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_buffer - Prints the content of a buffer
+ * @b: The buffer to print
+ * @size: The size of the buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+int i, j;
+
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+for (i = 0; i < size; i += 10)
+{
+printf("%08x: ", i);
+
+for (j = 0; j < 10; j++)
+{
+if (i + j < size)
+printf("%02x", *(b + i + j));
+else
+printf("  ");
+if (j % 2)
+printf(" ");
+}
+for (j = 0; j < 10; j++)
+{
+if (i + j >= size)
+break;
+if (*(b + i + j) >= 32 && *(b + i + j) <= 126)
+printf("%c", *(b + i + j));
+else
+printf(".");
+}
+printf("\n");
+}
+}

f6c98ad39d6ec16e08c4dc9edc4bf03a47217ce4
xola-mthembu <xola.mthembu@gmail.com>
2023-07-27 09:52:59 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: The character to be written
+ * _putchar - Writes a character to stdout.
+ * @c: The character to be written.
  *
- * Return: On success, the number of characters written.
- * On error, -1 is returned and errno is set appropriately.
+ * Return: On success, 1. On error, -1.
  */
 int _putchar(char c)
 {

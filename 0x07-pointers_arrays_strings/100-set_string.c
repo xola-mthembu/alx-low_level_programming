b7b337e337ab9ce61037ce5c722c96ca1660ee90
xola-mthembu <xola.mthembu@gmail.com>
2023-07-31 04:46:41 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - Sets the value of a pointer to a char.
+ * @s: Double pointer to the string to be updated.
+ * @to: Pointer to the string to be set.
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}

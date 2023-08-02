04230b23a71f8b90450d245c0a91f1daf0284f15
xola-mthembu <xola.mthembu@gmail.com>
2023-08-02 12:49:39 -0700

    3-factorial.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,7 @@ int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _putchar(char c);
 int _strlen_recursion(char *s);
+int _putchar(char c);
+int factorial(int n);
 
 #endif /* MAIN_H */

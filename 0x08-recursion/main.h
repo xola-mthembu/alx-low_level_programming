7153fc11c54d256f4ba192fae5ad2f7a1e352f4c
xola-mthembu <xola.mthembu@gmail.com>
2023-08-02 11:44:51 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,5 +6,7 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
+int _putchar(char c);
+int _strlen_recursion(char *s);
 
 #endif /* MAIN_H */

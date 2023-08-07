967bc6fdf21345cc4bd551d69729aafa6f5f3c97
xola-mthembu <xola.mthembu@gmail.com>
2023-08-07 13:42:23 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
+char *array;
+unsigned int i;
 if (size == 0)
 return (NULL);
-char *array = (char *)malloc(sizeof(char) * size);
+array = (char *)malloc(sizeof(char) * size);
 if (array == NULL)
 return (NULL);
-unsigned int i;
 for (i = 0; i < size; i++)
 {
 array[i] = c;

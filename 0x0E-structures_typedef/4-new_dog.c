000a50669c66a21cd03229a97cddd5c6c92de6fc
xola-mthembu <xola.mthembu@gmail.com>
2023-08-14 15:00:16 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
+/**
+ * new_dog - Creates a new dog with given information
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the dog's owner
+ *
+ * Return: Pointer to the new dog_t structure,
+ * or NULL if memory allocation fails
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog;

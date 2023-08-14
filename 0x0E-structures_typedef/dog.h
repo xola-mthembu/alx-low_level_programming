b5e3ba89f279b255a2a52c6db0737bf6ac553918
xola-mthembu <xola.mthembu@gmail.com>
2023-08-14 13:33:26 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+ * struct dog - A structure representing a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+struct dog
+{
 char *name;
 float age;
 char *owner;

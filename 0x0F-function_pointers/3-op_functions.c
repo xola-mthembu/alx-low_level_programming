ec9251d007679afb8885e5b6a1dda7f23b60b551
xola-mthembu <xola.mthembu@gmail.com>
2023-08-16 12:54:53 -0700

    Q3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Sum of a and b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - Subtracts two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Difference of a and b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Product of a and b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - Divides two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of division of a by b
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - Modulo operation on two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Remainder of division of a by b
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
-int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
 #endif /* FUNCTION_POINTERS_H */

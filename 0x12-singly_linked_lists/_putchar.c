f6febf86bf753bc0a2641da04b8cbd1540ab0e7c
xola-mthembu <xola.mthembu@gmail.com>
2023-08-24 13:22:51 -0700

    Print list

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - Writes a character to the standard output.
+ * @c: The character to be written.
+ * Return: On success, the number of characters written.
+ *         On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}

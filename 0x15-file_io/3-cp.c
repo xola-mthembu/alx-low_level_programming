4279e16ff825b8834faf622e63004c56725ceb77
xola-mthembu <xola.mthembu@gmail.com>
2023-11-27 14:13:09 -0800

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,56 +1,108 @@
-#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define BUF_SIZE 1024
+void close_file(int fd);
+int open_source_file(const char *filename);
+int open_dest_file(const char *filename);
+void copy_content(int file_from, int file_to, const char *src, const char *dest);
 /**
- * print_error - Print an error message to standard error.
- * @exit_code: The exit code.
- * @format: The format string for the error message.
- *
- * Return: void
+ * close_file - Closes a file descriptor and prints an error if it fails.
+ * @fd: The file descriptor to close.
  */
-void print_error(int exit_code, const char *format, ...)
+void close_file(int fd)
 {
-	va_list args;
-
-	va_start(args, format);
-	dprintf(STDERR_FILENO, format, args);
-	va_end(args);
-
-	exit(exit_code);
+	if (close(fd) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 /**
- * main - Entry point for the program.
- * @ac: The number of command-line arguments.
- * @av: An array of strings containing the command-line arguments.
+ * open_source_file - Opens a file to read from.
+ * @filename: The name of the file to open.
  *
- * Return: 0 on success, other values on failure.
+ * Return: The file descriptor for the opened file.
  */
-int main(int ac, char **av)
+int open_source_file(const char *filename)
 {
-	int file_from, file_to, read_status, write_status;
-	char buffer[1024];
+	int fd = open(filename, O_RDONLY);
 
-	if (ac != 3)
-		print_error(97, "Usage: %s file_from file_to\n", av[0]);
-
-	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
-		print_error(98, "Error: Can't read from file %s\n", av[1]);
-
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
-		print_error(99, "Error: Can't write to %s\n", av[2]);
-
-	while ((read_status = read(file_from, buffer, 1024)) > 0)
+	if (fd < 0)
 	{
-		write_status = write(file_to, buffer, read_status);
-		if (write_status == -1 || (size_t)write_status != (size_t)read_status)
-			print_error(99, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (fd);
+}
+/**
+ * open_dest_file - Opens a file to write to, creating it if necessary.
+ * @filename: The name of the file to open.
+ *
+ * Return: The file descriptor for the opened file.
+ */
+int open_dest_file(const char *filename)
+{
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (fd);
+}
+/**
+ * copy_content - Reads content from one file and writes it to another.
+ * @file_from: The file descriptor to read from.
+ * @file_to: The file descriptor to write to.
+ * @src: Source filename for error messages.
+ * @dest: Destination filename for error messages.
+ */
+void copy_content(int file_from, int file_to, const char *src, const char *dest)
+{
+	int read_res, write_res;
+	char buf[BUF_SIZE];
+
+	while ((read_res = read(file_from, buf, BUF_SIZE)) > 0)
+	{
+		write_res = write(file_to, buf, read_res);
+		if (write_res != read_res)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+			exit(99);
+		}
 	}
 
-	if (read_status == -1)
-		print_error(98, "Error: Can't read from file %s\n", av[1]);
+	if (read_res < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+}
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to;
 
-	if (close(file_from) == -1 || close(file_to) == -1)
-		print_error(100, "Error: Can't close fd %d\n", (file_from == -1) ? file_to : file_from);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open_source_file(argv[1]);
+	file_to = open_dest_file(argv[2]);
+	copy_content(file_from, file_to, argv[1], argv[2]);
+	close_file(file_from);
+	close_file(file_to);
 
 	return (0);
 }

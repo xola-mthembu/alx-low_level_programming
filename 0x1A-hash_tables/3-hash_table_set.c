b72ca700ec75093f2fc6d93370300364f7a09324
xola-mthembu <xola.mthembu@gmail.com>
2024-02-02 12:30:08 -0800

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table you want to add or update the key/value to.
+ * @key: The key. Key cannot be an empty string.
+ * @value: The value associated with the key. Value must be duplicated.
+ *         Value can be an empty string.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *temp;
+
+	if (!ht || !key || *key == '\0' || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}

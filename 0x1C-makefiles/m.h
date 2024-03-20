71a554d538d2f52054a36e319eeffe905f92f21e
xola-mthembu <xola.mthembu@gmail.com>
2024-03-20 10:04:50 -0700

    Task 0 - 0-Makefile

---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,6 @@
+#ifndef M_H
+#define M_H
+
+/* Function prototypes here */
+
+#endif /* M_H */
---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,9 @@
+/**
+ * function_name - a function that does nothing (stub)
+ *
+ * Return: 0
+ */
+int function_name(void)
+{
+	return (0);
+}
